eef06409551ea939a131bfee5a1bdb95ad8c8805
DSallee <dsallee.dev@gmail.com>
2017-03-07 11:18:27 +0100

    correction

--- client.py
+++ client.py
@@ -4,24 +4,23 @@
 
 import socket
 
-HOST = 'localhost'   # Symbolic name, meaning all available interfaces
+HOST = 'serveur'   # Symbolic name, meaning all available interfaces
 PORT = 1717 # Arbitrary non-privileged port
-
-server_cn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
 
-server_cn.connect((HOST, PORT))
-print 'Connection established with the server on port {}'.format(PORT))
+sock.connect((HOST, PORT))
+print 'Connection established with the server on port {}'.format(PORT, HOST)
 
-msg_to_send = b""
-while msg_to_send != b"end":
-    msg_to_send = input('> ')
-    # Peut planter si vous tapez des caractères spéciaux
-    msg_to_send = msg_to_send.encode()
-    # On envoie le message
-    server_cn.send(msg_to_send)
-    msg_received = server_cn.recv(1717)
-    print msg_received.decode()) # Là encore, peut planter s'il y a des accents
+while true:
+    command = sock.recv(PORT)
 
-print 'Closing connection'
-server_cn.close()
+    if command == 'quit':
+    print 'Closing connection'
+    sock.close()
+    break
+
+def Main ():
+    Connect()
+
+Main()
